dacbadb79de9bc71a15339be813076173b1a8140
Simon Cook <simon.cook@embecosm.com>
2013-02-25 17:12:21 +0000

    Correct branch names

--- arc-versions.sh
+++ arc-versions.sh
@@ -65,9 +65,9 @@ done
 # the development versions for the ARC 4.4 tool chain release. Only actually
 # matters if --auto-checkout is set.
 cgen="cgen:arc_4_4-cgen-1_0-dev"
-binutils="binutils:arc_4_8-binutils-2.23.1-dev"
-gcc="gcc:arc_4_8-gcc-4.8-dev"
-gdb="gdb:arc_4_8-gdb-7.5.1-dev"
+binutils="binutils:arc_4_8-binutils-2_23_1-dev"
+gcc="gcc:arc_4_8-gcc-4_8-dev"
+gdb="gdb:arc_4_8-gdb-7_5_1-dev"
 newlib="newlib:arc_4_8-newlib-2_0-dev"
 uclibc="uClibc:arc_4_8-uClibc-0_9_30-dev"
 linux="linux:stable-arc-3.2"
