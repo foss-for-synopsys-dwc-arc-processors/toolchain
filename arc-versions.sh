e58524fad1e96c6640815ed8f94ef1c49e974b60
Jeremy Bennett <jeremy.bennett@embecosm.com>
2013-06-12 14:31:45 +0100

    * arc-versions.sh: Use LINUXDIR environment variable.
    Conflicts (resolved):
    	arc-versions.sh

--- arc-versions.sh
+++ arc-versions.sh
@@ -2,7 +2,7 @@
 
 # Script to specify versions of tools to use.
 
-# Copyright (C) 2012 Synopsys Inc.
+# Copyright (C) 2012, 2013 Synopsys Inc.
 
 # Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
 
@@ -29,6 +29,9 @@
 
 # The environment variable ${ARC_GNU} should point to the directory within
 # which the GIT trees live.
+
+# The environment variable ${LINUXDIR} should point to the Linux root
+# directory.
 
 # We checkout the desired branch for each tool. Note that these must exist or
 # we fail.
@@ -74,19 +77,56 @@ newlib="newlib:arc-newlib-mainline"
 uclibc="uClibc:arc_4_8-uClibc-0_9_30-dev"
 linux="linux:stable-arc-3.2"
 
+# We have to deal with some awkward cases here, because we have to deal with
+# the possibility that we may currently be on a detached HEAD (so cannot
+# fetch), or we will to checkout a detached HEAD (e.g. a tag). We also need to
+# deal with the case that the branch we wish to checkout is not yet in the
+# local repo, so we need to fetch before checking out.
+
+# The particularly awkward case is when we are detached, and want to checkout
+# a branch which is not yet in the local repo. In this case we must checkout
+# some other branch, then fetch, then checkout the branch we want. This has a
+# performance penalty, but only when coming from a detached branch.
+
+# In summary the steps are:
+# 1. If we are in detached HEAD state, checkout some arbitrary branch.
+# 2. Fetch (in case new branch)
+# 3. Checkout the branch
+# 4. Pull unless we are in a detached HEAD state.
+
+# Steps 1, 2 and 4 are only used if we have --auto-pull enabled.
+
+# All this will go horribly wrong if you leave uncommitted changes lying
+# around or if you change the remote. Nothing then but to sort it out by hand!
 for version in ${cgen} ${binutils} ${gcc} ${gdb} ${newlib} ${uclibc} ${linux}
 do
     tool=`echo ${version} | cut -d ':' -f 1`
     branch=`echo ${version} | cut -d ':' -f 2`
 
-    cd ${ARC_GNU}/${tool}
+    echo "Checking out branch/tag ${branch} of ${tool}"
+
+    # Kludge, because Linux has its own environment variable
+    if [ tool = "linux" ]
+    then
+	cd ${LINUXDIR}
+    else
+	cd ${ARC_GNU}/${tool}
+    fi
 
     if [ "x${autopull}" = "x--auto-pull" ]
     then
-	# Need to fetch first, in case it is a branch that is new. But only do
-	# this if we have auto-pull enabled (so we can still work if not
-	# online). Assumes the remote has not changed (if it has, you'll need
-	# to sort it out by hand).
+	if git branch | grep '\* (no branch)' > /dev/null 2>&1
+	then
+	    # Detached head. Checkout an arbitrary branch
+	    arb_br=`git branch | grep -v '^\*' | head -1`
+	    echo "  detached HEAD, interim checkout of ${arb_br}"
+	    if ! git checkout ${arb_br} > /dev/null 2>&1
+	    then
+		exit 1
+	    fi
+	fi
+	# Fetch any new branches
+	echo "  fetching branches"
 	if ! git fetch
 	then
 	    exit 1
@@ -101,6 +141,7 @@ do
 
     if [ "x${autocheckout}" = "x--auto-checkout" ]
     then
+	echo "  checking out ${branch}"
 	if ! git checkout ${branch}
 	then
 	    exit 1
@@ -109,9 +150,14 @@ do
 
     if [ "x${autopull}" = "x--auto-pull" ]
     then
-	if ! git pull
+	if ! git branch | grep '\* (no branch)' >> /dev/null 2>&1
 	then
-	    exit 1
+	    # Only update to latest if we are not in detached HEAD mode.
+	    echo "  pulling latest version"
+	    if ! git pull
+	    then
+		exit 1
+	    fi
 	fi
     fi
 done
