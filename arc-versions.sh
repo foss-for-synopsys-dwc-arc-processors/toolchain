c627224945c32a243228c7c83a4d674fffa74bc9
Simon Cook <simon.cook@embecosm.com>
2013-07-19 17:24:54 +0100

    Create arc-versions.sh for tag arc_4_8-R1.1

--- arc-versions.sh
+++ arc-versions.sh
@@ -69,12 +69,12 @@ done
 # Specify the default versions to use as a string <tool>:<branch>. These are
 # the development versions for the ARC 4.4 tool chain release. Only actually
 # matters if --auto-checkout is set.
-cgen="cgen:arc_4_8-cgen-1_0-stable"
-binutils="binutils:arc_4_8-binutils-2_23_2-stable"
-gcc="gcc:arc_4_8-gcc-4_8-stable"
-gdb="gdb:arc_4_8-gdb-7_5_1-stable"
-newlib="newlib:arc_4_8-newlib-2_0-stable"
-uclibc="uClibc:arc_4_8-uclibc-stable"
+cgen="cgen:arc_4_8-R1.1"
+binutils="binutils:arc_4_8-R1.1"
+gcc="gcc:arc_4_8-R1.1"
+gdb="gdb:arc_4_8-R1.1"
+newlib="newlib:arc_4_8-R1.1"
+uclibc="uClibc:arc_4_8-R1.1"
 linux="linux:arc-3.9"
 
 # We have to deal with some awkward cases here, because we have to deal with
