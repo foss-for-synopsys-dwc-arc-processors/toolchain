ff02eb59c30b20a231fc7d1f620158142969c3e2
Vineet Gupta <vgupta@synopsys.com>
2013-08-29 14:03:35 +0530

    * arc-versions.sh: Update uClibc branch name to new naming convention.
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- arc-versions.sh
+++ arc-versions.sh
@@ -74,7 +74,7 @@ binutils="binutils:arc-2.23-dev"
 gcc="gcc:arc-4.8-dev"
 gdb="gdb:arc-7.5-dev"
 newlib="newlib:arc-2.0-dev"
-uclibc="uClibc:arc_4_8-uclibc-mainline-dev"
+uclibc="uClibc:arc-mainline-dev"
 linux="linux:arc-3.9"
 
 # We have to deal with some awkward cases here, because we have to deal with
