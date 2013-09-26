82798fa4d402f2ab9197f815a1d41841596c96f7
Anton Kolesov <akolesov@synopsys.com>
2013-09-26 20:09:34 +0400

    Fix double usage of same variable name
    Signed-off-by: Anton Kolesov <akolesov@synopsys.com>
    (cherry picked from commit 70e9b9a8e5a45e00b20a6bab84dea77b025d6699)

--- arc-versions.sh
+++ arc-versions.sh
@@ -40,7 +40,7 @@
 # Default options
 autocheckout="--auto-checkout"
 autopull="--auto-pull"
-uclibc="--uclibc"
+uclibc_arg="--uclibc"
 
 # Parse options
 until
@@ -55,7 +55,7 @@ case ${opt} in
 	;;
 
     --uclibc | --no-uclibc)
-	uclibc=$1
+	uclibc_arg=$1
 	;;
 
     ?*)
@@ -83,7 +83,7 @@ gdb="gdb:arc-7.5-dev"
 newlib="newlib:arc-2.0-dev"
 uclibc="uClibc:arc-mainline-dev"
 
-if [ "x${uclibc}" = "x--uclibc" ]
+if [ "x${uclibc_arg}" = "x--uclibc" ]
 then
     linux="linux:arc-3.9"
 else
