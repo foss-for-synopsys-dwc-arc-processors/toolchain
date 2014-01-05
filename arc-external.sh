67601cc716832f529a2aaa94639c7911afe28dfb
Anton Kolesov <anton.kolesov@synopsys.com>
2014-01-05 20:30:38 +0400

    Update versions of downloadable dependencies
    Versions changes: GMP 4.3.2 -> 5.1.3, MPFR 2.4.2 -> 3.1.2.
    
    Fix invalid date in changelog for previous commit.
    
    Signed-off-by: Anton Kolesov <anton.kolesov@synopsys.com>

--- arc-external.sh
+++ arc-external.sh
@@ -3,8 +3,8 @@
 # Download some external dependencies. Return non-zero status if there is an error.
 
 urls='
-ftp://ftp.gmplib.org/pub/gmp/gmp-4.3.2.tar.bz2
-http://www.mpfr.org/mpfr-2.4.2/mpfr-2.4.2.tar.bz2
+ftp://ftp.gmplib.org/pub/gmp/gmp-5.1.3.tar.bz2
+http://www.mpfr.org/mpfr-3.1.2/mpfr-3.1.2.tar.bz2
 http://www.multiprecision.org/mpc/download/mpc-1.0.1.tar.gz
 '
 
@@ -12,7 +12,7 @@ for url in ${urls} ; do
     filename="$(echo "${url}" | sed 's/^.*\///')"
     dirname="$(echo "${filename}" | sed 's/\.tar\..*$//')"
     toolname="$(echo "${dirname}" | cut -d- -f1)"
-    
+
     if echo "${filename}" | grep -q .tar.bz2 ; then
         tar="tar xjf"
     else
