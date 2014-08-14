a913b5210158bde989946a3133e71fe375fdccd0
Anton Kolesov <Anton.Kolesov@synopsys.com>
2014-08-14 19:50:05 +0400

    windows: Copy Eclipse instead of moving
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- windows-installer/build-installer.sh
+++ windows-installer/build-installer.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # It is assumed that current directory is directory where with script is
 # located. It is also assumed that all prerequisites are downloaded to the
@@ -10,6 +10,7 @@ mkdir in
 cd in
 
 # Copy MinGW and MSYS runtime files
+echo "Copying MSYS and MinGW runtime files..."
 tar xaf ../tars/coreutils-*-msys-*-bin.tar.lzma
 tar xaf ../tars/gcc-core-*-mingw32-dll.tar.lzma
 tar xaf ../tars/gettext-*-mingw32-dll.tar.lzma
@@ -21,27 +22,35 @@ tar xaf ../tars/make-*-mingw32-cvs-*-bin.tar.lzma
 mv bin/{mingw32-,}make.exe
 
 # Copy arcshell.bat
+echo "Copying arcshell.bat..."
 cp ../tars/arcshell.bat .
 
 # Copy OpenOCD
+echo "Copying OpenOCD..."
 tar xaf ../tars/openocd-ide-*.tgz --strip-components=1
 
 # Copy tool chain
-tar xaf ../tars/toolchain-ide-*.tgz --strip-components=1
-tar xaf ../tars/toolchain-ide-*_eb.tgz --strip-components=1
+echo "Copything toolchain..."
+tar xaf ../tars/arc_gnu_*_prebuilt_elf32_windows_install.tgz --strip-components=1
 
 # Copy Eclipse
-mv ../tars/eclipse .
+echo "Copying Eclipse..."
+rsync -a ../tars/eclipse .
 
 # Copy Java runtime environment:
+echo "Copying JRE..."
 mkdir eclipse/jre
 cd eclipse/jre
-tar xaf jre-*-windows-i586.tar.gz --strip-components=1
+tar xaf ../../../tars/jre-*-windows-i586.tar.gz --strip-components=1
 cd ../../..
 
 # Generate installer an uninstaller sections
+echo "Generating nsis files..."
 ./gen-nsis-sections.sh
 
 # Generate installer
+echo "Creating installer..."
 /cygdrive/c/Program\ Files\ \(x86\)/NSIS/makensis.exe installer-standard.nsi
+
+echo "Done"
 
