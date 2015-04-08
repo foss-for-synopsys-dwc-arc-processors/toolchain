20ad8ce121a08f25c4f8b0a341d0553e6065af4a
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-04-08 20:21:14 +0300

    Switch to GDB 7.9
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -1,7 +1,7 @@
 cgen=cgen:arc-1.0-dev
 binutils=binutils:arc-2.23-dev
 gcc=gcc:arc-4.8-dev
-gdb=gdb:arc-7.5-dev
+gdb=gdb:arc-7.9-dev
 newlib=newlib:arc-2.0-dev
 uclibc=uClibc:arc-mainline-dev
 linux=linux:arc-3.13
