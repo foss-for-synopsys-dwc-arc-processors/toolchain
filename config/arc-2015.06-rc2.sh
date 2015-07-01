238c15189d4e608c4e25370129a7e32e295b08db
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-07-01 14:23:07 +0300

    Create arc-versions.sh for tag arc-2015.06-rc2

---
+++ config/arc-2015.06-rc2.sh
@@ -0,0 +1,7 @@
+cgen=cgen:arc-2015.06-rc2
+binutils=binutils:arc-2015.06-rc2
+gcc=gcc:arc-2015.06-rc2
+gdb=gdb:arc-2015.06-rc2-gdb
+newlib=newlib:arc-2015.06-rc2
+uclibc=uClibc:arc-2015.06-rc2
+linux=linux:arc-gnu-2015.06-rc2
