0984cc64b4110445f71b3048c884d5378deab410
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-11-12 21:20:28 +0300

    Switch to newlib/arc-2.3
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -2,6 +2,6 @@ cgen=cgen:arc-1.0-dev
 binutils=binutils:arc-2.23-dev
 gcc=gcc:arc-4.8-dev
 gdb=gdb:arc-7.9-dev
-newlib=newlib:arc-2.2-dev
+newlib=newlib:arc-2.3
 uclibc=uClibc:arc-mainline-dev
 linux=linux:arc-3.18
