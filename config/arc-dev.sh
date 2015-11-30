f53a95881acfd4f5b4e816b7959ac0a613a6957b
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-11-30 19:44:53 +0300

    Switch to GDB 7.10
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -1,7 +1,7 @@
 cgen=cgen:arc-1.0-dev
 binutils=binutils:arc-2.23-dev
 gcc=gcc:arc-4.8-dev
-gdb=gdb:arc-7.9-dev
+gdb=gdb:arc-7.10
 newlib=newlib:arc-2.3
 uclibc=uClibc:arc-mainline-dev
 linux=linux:arc-3.18
