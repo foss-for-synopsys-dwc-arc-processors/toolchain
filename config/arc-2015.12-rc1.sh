608b3c981fccdb60ee21c27efb45f4e0192675a2
Anton Kolesov <Anton.Kolesov@synopsys.com>
2015-12-24 21:43:55 +0300

    Create arc-versions.sh for tag arc-2015.12-rc1

---
+++ config/arc-2015.12-rc1.sh
@@ -0,0 +1,7 @@
+cgen=cgen:arc-2015.12-rc1
+binutils=binutils:arc-2015.12-rc1
+gcc=gcc:arc-2015.12-rc1
+gdb=gdb:arc-2015.12-rc1-gdb
+newlib=newlib:arc-2015.12-rc1
+uclibc=uClibc:arc-2015.12-rc1
+linux=linux:arc-gnu-2015.12-rc1
