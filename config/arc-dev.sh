b999521b4a6ea76d67e7e627adba38aa5971ae22
Anton Kolesov <Anton.Kolesov@synopsys.com>
2016-05-05 13:05:38 +0300

    Update to newlib 2.4
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -2,6 +2,6 @@ cgen=cgen:arc-1.0-dev
 binutils=binutils:arc-2.26-dev
 gcc=gcc:arc-4.8-dev
 gdb=gdb:arc-7.10
-newlib=newlib:arc-2.3
+newlib=newlib:arc-2.4
 uclibc=uClibc:arc-mainline-dev
 linux=linux:linux-4.5.y
