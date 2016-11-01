d10851858ca20accec0f3e54f705aab5b65e3137
Anton Kolesov <Anton.Kolesov@synopsys.com>
2016-11-01 18:13:15 +0300

    Create arc-versions.sh for tag arc-2016.09-eng019

---
+++ config/arc-2016.09-eng019.sh
@@ -0,0 +1,6 @@
+binutils=binutils:arc-2016.09-eng019
+gcc=gcc:arc-2016.09-eng019
+gdb=gdb:arc-2016.09-eng019-gdb
+newlib=newlib:arc-2016.09-eng019
+uclibc=uClibc:arc-2016.09-eng019
+linux=linux:linux-4.8.y
