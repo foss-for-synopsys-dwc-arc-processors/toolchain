586a6fb22b7d4249e0dfe7162718b0d6cca3a3d0
Anton Kolesov <Anton.Kolesov@synopsys.com>
2017-01-30 13:29:45 +0300

    Create arc-versions.sh for tag arc-2017.03-eng005

---
+++ config/arc-2017.03-eng005.sh
@@ -0,0 +1,6 @@
+binutils=binutils:arc-2017.03-eng005
+gcc=gcc:arc-2017.03-eng005
+gdb=gdb:arc-2017.03-eng005-gdb
+newlib=newlib:arc-2017.03-eng005
+uclibc=uClibc:arc-2017.03-eng005
+linux=linux:linux-4.8.y
