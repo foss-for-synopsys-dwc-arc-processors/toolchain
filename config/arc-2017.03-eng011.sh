6e6cfecea212091d0e0c119a5a3057391b1eee0b
ARC GNU Verification <arcgnu_verif@synopsys.com>
2017-03-23 20:05:38 +0300

    Create arc-versions.sh for tag arc-2017.03-eng011

---
+++ config/arc-2017.03-eng011.sh
@@ -0,0 +1,6 @@
+binutils=binutils:arc-2017.03-eng011
+gcc=gcc:arc-2017.03-eng011
+gdb=gdb:arc-2017.03-eng011-gdb
+newlib=newlib:arc-2017.03-eng011
+uclibc=uClibc:arc-2017.03-eng011
+linux=linux:linux-4.9.y
