d4d7cf8f5684a56175fdcb427c72b61d27765947
ARC GNU Verification <arcgnu_verif@synopsys.com>
2017-10-19 14:10:54 +0300

    Create arc-versions.sh for tag arc-2017.09-rc2

---
+++ config/arc-2017.09-rc2.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2017.09-rc2
+gcc=gcc:arc-2017.09-rc2
+gdb=gdb:arc-2017.09-rc2-gdb
+glibc=glibc:arc-2017.09-rc2
+newlib=newlib:arc-2017.09-rc2
+uclibc=uClibc:arc-2017.09-rc2
+linux=linux:linux-4.12.y
