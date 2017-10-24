d552b2fe525c496a5f5c96542ba8d95420a201b8
Anton Kolesov <Anton.Kolesov@synopsys.com>
2017-10-24 15:43:34 +0300

    Remove 2017.09-release
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- config/arc-2017.09-release.sh
+++
@@ -1,7 +0,0 @@
-binutils=binutils:arc-2017.09-release
-gcc=gcc:arc-2017.09-release
-gdb=gdb:arc-2017.09-release-gdb
-glibc=glibc:arc-2017.09-release
-newlib=newlib:arc-2017.09-release
-uclibc=uClibc:arc-2017.09-release
-linux=linux:linux-4.12.y
