13570ab79cc7cc48a52b6746bd6fb81920dcd04a
Anton Kolesov <Anton.Kolesov@synopsys.com>
2017-11-01 17:25:33 +0300

    Move to arc-2018.03 branches
    Signed-off-by: Anton Kolesov <Anton.Kolesov@synopsys.com>

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -1,7 +1,7 @@
-binutils=binutils:arc-2017.09
-gcc=gcc:arc-2017.09
-gdb=gdb:arc-2017.09-gdb
-newlib=newlib:arc-2017.09
+binutils=binutils:arc-2018.03
+gcc=gcc:arc-2018.03
+gdb=gdb:arc-2018.03-gdb
+newlib=newlib:arc-2018.03
 uclibc=uClibc:arc-2017.09
 linux=linux:linux-4.12.y
 glibc=glibc:vineet-glibc-master
