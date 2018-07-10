51b87553800e9b53f32169b3a8032a3ec1d0e275
Anton Kolesov <Anton.Kolesov@synopsys.com>
2018-07-10 19:46:12 +0300

    Switch to 2018.09 branches

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -1,7 +1,7 @@
-binutils=binutils:arc-2018.03
-gcc=gcc:arc-2018.03
-gdb=gdb:arc-2018.03-gdb
-newlib=newlib:arc-2018.03
+binutils=binutils:arc-2018.09
+gcc=gcc:arc-2018.09
+gdb=gdb:arc-2018.09-gdb
+newlib=newlib:arc-2018.09
 uclibc=uclibc-ng:v1.0.27
 linux=linux:linux-4.15.y
 glibc=glibc:vineet-glibc-master
