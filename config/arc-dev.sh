29c34992519744717d13b3c88d12d2f26d3a8a0c
Anton Kolesov <Anton.Kolesov@synopsys.com>
2018-10-02 19:27:03 +0300

    Update to uClibc-ng 1.0.29

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -2,6 +2,6 @@ binutils=binutils:arc-2018.09
 gcc=gcc:arc-2018.09
 gdb=gdb:arc-2018.09-gdb
 newlib=newlib:arc-2018.09
-uclibc=uclibc-ng:v1.0.27
+uclibc=uclibc-ng:v1.0.29
 linux=linux:linux-4.15.y
 glibc=glibc:vineet-glibc-master
