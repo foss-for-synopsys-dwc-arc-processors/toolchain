3db965a05d5959cfd0be736460a6944f54709d57
ARC GNU Verification <arcgnu_verif@synopsys.com>
2018-11-21 14:48:49 +0300

    Create arc-versions.sh for tag arc-2018.09-release

---
+++ config/arc-2018.09-release.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2018.09-release
+gcc=gcc:arc-2018.09-release
+gdb=gdb:arc-2018.09-release-gdb
+glibc=glibc:arc-2018.09-release
+newlib=newlib:arc-2018.09-release
+uclibc=uclibc-ng:arc-2018.09-release
+linux=linux:linux-4.15.y
