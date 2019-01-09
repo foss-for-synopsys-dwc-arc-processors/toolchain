dd39c116cbec98744715a2c77eda51f5b81f979b
Anton Kolesov <Anton.Kolesov@synopsys.com>
2019-01-09 21:59:48 +0300

    Switch to arc-2019.03 branches where available

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -1,7 +1,7 @@
-binutils=binutils:arc-2018.09
+binutils=binutils:arc-2019.03
 gcc=gcc:arc-2018.09
-gdb=gdb:arc-2018.09-gdb
-newlib=newlib:arc-2018.09
+gdb=gdb:arc-2019.03-gdb
+newlib=newlib:arc-2019.03
 uclibc=uclibc-ng:v1.0.29
 linux=linux:linux-4.15.y
 glibc=glibc:arc-glibc-2.28
