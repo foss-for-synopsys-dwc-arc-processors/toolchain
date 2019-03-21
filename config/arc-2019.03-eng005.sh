ca5c735eb9d5a37f8eb471b2cd8a01800e77fd2a
ARC GNU Verification <arcgnu_verif@synopsys.com>
2019-03-21 23:44:33 +0300

    Create arc-versions.sh for tag arc-2019.03-eng005

---
+++ config/arc-2019.03-eng005.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2019.03-eng005
+gcc=gcc:arc-2019.03-eng005
+gdb=gdb:arc-2019.03-eng005-gdb
+glibc=glibc:arc-2019.03-eng005
+newlib=newlib:arc-2019.03-eng005
+uclibc=uclibc-ng:arc-2019.03-eng005
+linux=linux:linux-4.15.y
