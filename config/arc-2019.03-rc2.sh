a9755405d4c7c4055fd2cd85914c54a58e72df58
ARC GNU Verification <arcgnu_verif@synopsys.com>
2019-05-23 20:10:47 +0300

    Remove 2019.03-rc2 config file
    Required to recreate git tags for the release.

--- config/arc-2019.03-rc2.sh
+++
@@ -1,7 +0,0 @@
-binutils=binutils:arc-2019.03-rc2
-gcc=gcc:arc-2019.03-rc2
-gdb=gdb:arc-2019.03-rc2-gdb
-glibc=glibc:arc-2019.03-rc2
-newlib=newlib:arc-2019.03-rc2
-uclibc=uclibc-ng:v1.0.31
-linux=linux:linux-4.15.y
