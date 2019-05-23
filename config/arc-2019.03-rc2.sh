259ac0a927927ee43b48612a5148968f82f79650
ARC GNU Verification <arcgnu_verif@synopsys.com>
2019-05-23 20:22:12 +0300

    Create arc-versions.sh for tag arc-2019.03-rc2

---
+++ config/arc-2019.03-rc2.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2019.03-rc2
+gcc=gcc:arc-2019.03-rc2
+gdb=gdb:arc-2019.03-rc2-gdb
+glibc=glibc:arc-2019.03-rc2
+newlib=newlib:arc-2019.03-rc2
+uclibc=uclibc-ng:v1.0.31
+linux=linux:linux-4.15.y
