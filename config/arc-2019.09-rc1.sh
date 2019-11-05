6ea8f4024d254d7064d342460c24c44f82df60fd
ARCOSS Verification Team <arcoss-verification-team@synopsys.com>
2019-11-05 18:52:50 +0300

    Create arc-versions.sh for tag arc-2019.09-rc1

---
+++ config/arc-2019.09-rc1.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2019.09-rc1
+gcc=gcc:arc-2019.09-rc1
+gdb=gdb:arc-2019.09-rc1-gdb
+glibc=glibc:arc-2019.09-rc1
+newlib=newlib:arc-2019.09-rc1
+uclibc=uclibc-ng:v1.0.31
+linux=linux:linux-4.15.y
