6683d8bbcb1f11d8c3d3bd928e3477c24fbfd862
Mikhail Falaleev <falaleevms@gmail.com>
2020-03-19 22:55:07 +0300

    Switch to 2020.03 branches

--- config/arc-dev.sh
+++ config/arc-dev.sh
@@ -1,7 +1,7 @@
-binutils=binutils:arc-2019.09
-gcc=gcc:arc-2019.09
-gdb=gdb:arc-2019.09-gdb
-newlib=newlib:arc-2019.03
+binutils=binutils:arc-2020.03
+gcc=gcc:arc-2020.03
+gdb=gdb:arc-2020.03
+newlib=newlib:arc-2020.03
 uclibc=uclibc-ng:v1.0.31
 linux=linux:linux-4.15.y
 glibc=glibc:arc-glibc-master
