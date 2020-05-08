13d12b55770129bb6821823e6a3f2ecf05077a6a
Jingru <jingru@synopsys.com>
2020-05-08 16:47:34 +0300

    Create arc-versions.sh for tag arc-2020.03-eng005

---
+++ config/arc-2020.03-eng005.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2020.03-eng005
+gcc=gcc:arc-2020.03-eng005
+gdb=gdb:arc-2020.03-eng005-gdb
+glibc=glibc:arc-2020.03-eng005
+newlib=newlib:arc-2020.03-eng005
+uclibc=uclibc-ng:v1.0.31
+linux=linux:linux-4.15.y
