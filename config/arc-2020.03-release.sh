731a77e150f5a28dfdc4eac674fde3335242d7c1
Jingru <jingru@synopsys.com>
2020-05-24 23:49:32 +0300

    Create arc-versions.sh for tag arc-2020.03-release

---
+++ config/arc-2020.03-release.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2020.03-release
+gcc=gcc:arc-2020.03-release
+gdb=gdb:arc-2020.03-release-gdb
+glibc=glibc:arc-2020.03-release
+newlib=newlib:arc-2020.03-release
+uclibc=uclibc-ng:v1.0.31
+linux=linux:linux-4.15.y
