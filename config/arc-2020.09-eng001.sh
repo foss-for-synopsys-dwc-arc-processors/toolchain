f8f494c73c39d09082db1d24f25e2f46c0328bfd
synopsys-arcoss-auto <svc-arcoss_auto@synopsys.com>
2020-07-11 16:33:53 +0300

    Create arc-versions.sh for tag arc-2020.09-eng001

--- config/arc-2020.09-eng001.sh
+++ config/arc-2020.09-eng001.sh
@@ -3,5 +3,5 @@ gcc=gcc:arc-2020.09-eng001
 gdb=gdb:arc-2020.09-eng001-gdb
 glibc=glibc:arc-2020.09-eng001
 newlib=newlib:arc-2020.09-eng001
-uclibc=uclibc-ng:v1.0.31
+uclibc=uclibc-ng:v1.0.34
 linux=linux:linux-4.15.y
