709380d75923cf4c0c2323e38a288e6778f7fed4
synopsys-arcoss-auto <svc-arcoss_auto@synopsys.com>
2020-08-02 00:20:51 +0300

    Create arc-versions.sh for tag arc-2020.09-eng004

--- arc-versions.sh
+++ arc-versions.sh
@@ -73,7 +73,7 @@ done
 # That should be a separate variable to allow for a straightforward creation of
 # new releases, where we want default to point to release, instead of dev
 # branch.
-default_toolchain_config=arc-2020.09-eng003
+default_toolchain_config=arc-2020.09-eng004
 
 # Specify the default versions to use as a string <tool>:<branch>. Those are
 # taken from the checkout configuration file. Only actually matters if
