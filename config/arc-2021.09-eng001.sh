61387d84303308a530790004d3b4718e145fe382
synopsys-arcoss-auto <svc-arcoss_auto@synopsys.com>
2021-06-20 16:36:23 +0300

    Create arc-versions.sh for tag arc-2021.09-eng001

---
+++ config/arc-2021.09-eng001.sh
@@ -0,0 +1,7 @@
+binutils=binutils:arc-2021.09-eng001
+gcc=gcc:arc-2021.09-eng001
+gdb=gdb:arc-2021.09-eng001-gdb
+newlib=newlib:arc-2021.09-eng001
+glibc=glibc:glibc-2.33
+uclibc=uclibc-ng:v1.0.34
+linux=linux:v5.1
